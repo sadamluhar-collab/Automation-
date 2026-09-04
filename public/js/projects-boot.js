382065ef8c81f7ce1b332807ec76f31a2bbbd5b4
Saddam Luhar42 <sadamluhar@gmail.com>
2026-09-04 19:07:44 +0530

    Wire Projects workspace directly into navigation

--- public/js/projects-boot.js
+++ public/js/projects-boot.js
@@ -1,3 +1,1 @@
-import {activate} from './projects.js';
-
-document.querySelectorAll('#nav button[data-section="projects"]').forEach(button=>button.addEventListener('click',()=>activate()));
+document.querySelectorAll('#nav button[data-section="projects"]').forEach(button=>button.addEventListener('click',()=>window.dispatchEvent(new CustomEvent('automation:projects'))));
